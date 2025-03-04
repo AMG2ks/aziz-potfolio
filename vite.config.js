eb2c64ae2c1a1e691ebf1aae118e35512b61f98b
aziz.guebsi <aziz.gabsi@digitalberry.fr>
2025-03-04 18:09:17 +0100

    deploy logic

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/aziz-portfolio/',
+  base: '/aziz-potfolio/',
 })
